502d78a707f10fd09e79e8672e95b83caba44451
Devanoyudhistira <devanotira@gmail.com>
2024-06-10 20:16:08 +0700

    fixing

--- scripts/create.js
+++ scripts/create.js
@@ -6,7 +6,7 @@ export const close = document.querySelector("#closeform");
 export const notewrap = document.querySelector("#note-wrap");
 export const notew = document.querySelectorAll(".note");
 export const title = document.querySelector("#notetitle");
-export const work = document.querySelector("#work");
+export const work  = document.querySelector("#work");
 export const project = document.querySelector("#project");
 export const daily = document.querySelector("#daily");
 
@@ -20,17 +20,18 @@ close.addEventListener("click", (e) => {
 });
 
 form.addEventListener("submit", (e) => {
-  // e.preventDefault();
-  let i = 1;
-  let judulnote = title.value;
-  localStorage.setItem(`note`, judulnote);
+  e.preventDefault();
+  form.classList.replace("flex", "hidden");
+  const nama = title.value;
+  let key = `${new Date().getTime()}`;
+  let value = nama
+  localStorage.setItem(key,value);
   const notediv = document.createElement("div");
   notediv.classList = "note before:bg-yellow-500";
 
-  console.log(judulnote);
 
   notediv.innerHTML = `<div  class="flex flex-col">
-            <h1 class="judul-note"> ${judulnote} </h1>
+            <h1 class="judul-note"> ${value} </h1>
             <p id="date" class="ml-2 text-xl font-poppins font-[500]">
                 ${moment().format("YYYY MMMM DD hh:mm")}
             </p>
@@ -38,29 +39,37 @@ form.addEventListener("submit", (e) => {
           <button class=""> <i class="fa-solid fa-trash  text-3xl text-red-600"></i> </button>`;
 
   notewrap.appendChild(notediv);
-  // console.log(notew.length);
   document.querySelector("#date").textContent = moment().format(
     "YYYY MMMM DD   hh:mm"
   );
-
-  form.reset;
-});
-let notestore = localStorage.getItem("note");
-if (notestore) {
   document.querySelector("#empty").classList.replace("grid", "hidden");
-  const notediv = document.createElement("div");
-  notediv.classList = "note before:bg-yellow-500";
 
-  notediv.innerHTML = `<div  class="flex flex-col">
-            <h1 class="judul-note"> ${notestore} </h1>
-            <p id="date" class="ml-2 text-xl font-poppins font-[500]">
-                ${moment().format("YYYY MMMM DD hh:mm")}
-            </p>
-          </div>
-          <button class=""> <i class="fa-solid fa-trash  text-3xl text-red-600"></i> </button>`;
-
-  notewrap.appendChild(notediv);
-  document.querySelector("#date").textContent =
-    moment().format("YYYY MMMM DD HH:mm");
-}
- 
+  form.reset();
+  });
+  
+  for(let i = 0;i < localStorage.length;i++){
+    let userkey = localStorage.key(i)
+  
+    let store = localStorage.getItem(userkey)
+  
+    if(store){
+      document.querySelector("#empty").classList.replace("grid", "hidden");
+      console.log(store)
+      const notediv = document.createElement("div");
+      notediv.classList = "note bg-yellow-500";
+    
+    
+      notediv.innerHTML = `<div  class="flex flex-col">
+                <h1 class="judul-note"> ${store} </h1>
+                <p id="date" class="ml-2 text-xl font-poppins font-[500]">
+                    ${moment().format("YYYY MMMM DD hh:mm")}
+                </p>
+              </div>
+              <button class=""> <i class="fa-solid fa-trash  text-3xl text-red-600"></i> </button>`;
+    
+      notewrap.appendChild(notediv);
+      document.querySelector("#date").textContent = moment().format(
+        "YYYY MMMM DD   hh:mm"
+      );
+    }
+  }
